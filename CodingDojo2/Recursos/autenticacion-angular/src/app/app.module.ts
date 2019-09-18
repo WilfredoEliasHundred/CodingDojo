1ea2f8bfeab05b658d764429c4ef352181c0a749
Fernando Valverde Uchoffen <fvalverdeu@gmail.com>
2019-09-18 02:03:22 -0500

    frontend.

---
+++ CodingDojo2/Recursos/autenticacion-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UsuarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
